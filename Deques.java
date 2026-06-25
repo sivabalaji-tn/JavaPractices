b51b69191f26a476d674652ebc76b2f25aca12de
sivabalaji-tn <sivathetechie24@gmail.com>
2026-06-25 12:20:12 +0530

    Hello

--- Deques.java
+++ Deques.java
@@ -4,6 +4,10 @@ public class Deques{
         Deque<Integer> dq = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        for(int i=0; i<a; i++){
+            dq.offer(sc.nextInt());
+        }
+        dq.offer(90);
         
     }
 }
